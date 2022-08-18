d8603d1455afe8d02dac483a3a2d7defc396ebfd
Szymon SEMRAU <szymon.semrau@gmail.com>
2022-08-18 12:43:41 +0200

    initial

---
+++ src/CybsHttpClient.cs
@@ -0,0 +1,204 @@
+﻿namespace FlexApi.Demo
+{
+    using System;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Security.Cryptography;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class CybsHttpClient
+    {
+        private readonly string merchantId = "axa_retail_ecom_sit";
+
+        private readonly string runEnv = "apitest.cybersource.com";
+
+        private readonly string secretKey = <will be sent in separate message>;
+
+        private readonly string keyId = "3ea190d9-be22-46e6-b2e4-fc3cd8250ce4";
+
+        public string Post(string body, string resource, string contentType = "application/json")
+        {
+            string responseContent = string.Empty;
+            using (var client = new HttpClient())
+            {
+                using (var content = new StringContent(body))
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue(contentType);
+                    client.DefaultRequestHeaders.Add("v-c-merchant-id", this.merchantId);
+                    var gmtDateTime = DateTime.Now.ToUniversalTime().ToString("r");
+                    client.DefaultRequestHeaders.Add("Date", gmtDateTime);
+                    client.DefaultRequestHeaders.Add("Host", this.runEnv);
+                    var digest = GenerateDigest(body);
+                    client.DefaultRequestHeaders.Add("Digest", digest);
+                    StringBuilder signature = this.GenerateSignature(digest, gmtDateTime, "post", resource, this.merchantId);
+                    client.DefaultRequestHeaders.Add("Signature", signature.ToString());
+                    var response = client.PostAsync("https://" + this.runEnv + resource, content).Result;
+                    var responseCode = (TaskStatus)response.StatusCode;
+                    responseContent = response.Content.ReadAsStringAsync().Result;
+                }
+            }
+
+            return responseContent;
+        }
+
+        public string Get(string resource)
+        {
+            string gmtDateTime = DateTime.Now.ToUniversalTime().ToString("r");
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("v-c-merchant-id", merchantId);
+                client.DefaultRequestHeaders.Add("Date", gmtDateTime);
+                client.DefaultRequestHeaders.Add("Host", this.runEnv);
+
+                StringBuilder signature = this.GenerateSignature(gmtDateTime, resource, merchantId);
+                client.DefaultRequestHeaders.Add("Signature", signature.ToString());
+
+                var url = new Uri($"https://{this.runEnv}{resource}");
+                var response = client.GetAsync(url).Result;
+                var content = response.Content.ReadAsStringAsync().Result;
+                return content;
+            }
+        }
+
+        private StringBuilder GenerateSignature(string digest, string gmtDateTime, string method, string resource, string merchantId)
+        {
+            StringBuilder signatureHeaderValue = new StringBuilder();
+            string algorithm = "HmacSHA256";
+            string postHeaders = "host date (request-target) digest v-c-merchant-id";
+            string getHeaders = "host date (request-target) v-c-merchant-id";
+            string getRequestTarget = method + " " + resource;
+            string postRequestTarget = method + " " + resource;
+
+            try
+            {
+                StringBuilder signatureString = new StringBuilder();
+                signatureString.Append('\n');
+                signatureString.Append("host");
+                signatureString.Append(": ");
+                signatureString.Append(this.runEnv);
+                signatureString.Append('\n');
+                signatureString.Append("date");
+                signatureString.Append(": ");
+                signatureString.Append(gmtDateTime);
+                signatureString.Append('\n');
+                signatureString.Append("(request-target)");
+                signatureString.Append(": ");
+
+                if (method.Equals("post"))
+                {
+                    signatureString.Append(postRequestTarget);
+                    signatureString.Append('\n');
+                    signatureString.Append("digest");
+                    signatureString.Append(": ");
+                    signatureString.Append(digest);
+                }
+                else
+                {
+                    signatureString.Append(getRequestTarget);
+                }
+
+                signatureString.Append('\n');
+                signatureString.Append("v-c-merchant-id");
+                signatureString.Append(": ");
+                signatureString.Append(merchantId);
+                signatureString.Remove(0, 1);
+
+                byte[] signatureByteString = Encoding.UTF8.GetBytes(signatureString.ToString());
+
+                byte[] decodedKey = Convert.FromBase64String(this.secretKey);
+
+                HMACSHA256 aKeyId = new HMACSHA256(decodedKey);
+
+                byte[] hashmessage = aKeyId.ComputeHash(signatureByteString);
+                string base64EncodedSignature = Convert.ToBase64String(hashmessage);
+
+                signatureHeaderValue.Append("keyid=\"" + this.keyId + "\"");
+                signatureHeaderValue.Append(", algorithm=\"" + algorithm + "\"");
+
+                if (method.Equals("post"))
+                {
+                    signatureHeaderValue.Append(", headers=\"" + postHeaders + "\"");
+                }
+                else if (method.Equals("get"))
+                {
+                    signatureHeaderValue.Append(", headers=\"" + getHeaders + "\"");
+                }
+
+                signatureHeaderValue.Append(", signature=\"" + base64EncodedSignature + "\"");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR : " + ex.ToString());
+            }
+
+            return signatureHeaderValue;
+        }
+
+        private StringBuilder GenerateSignature(string gmtDateTime, string resource, string merchantId)
+        {
+            StringBuilder signatureHeaderValue = new StringBuilder();
+            string algorithm = "HmacSHA256";
+            string getHeaders = "host date (request-target) v-c-merchant-id";
+            string getRequestTarget = $"get {resource}";
+
+            StringBuilder signatureString = new StringBuilder();
+            signatureString.Append('\n');
+            signatureString.Append("host");
+            signatureString.Append(": ");
+            signatureString.Append(this.runEnv);
+            signatureString.Append('\n');
+            signatureString.Append("date");
+            signatureString.Append(": ");
+            signatureString.Append(gmtDateTime);
+            signatureString.Append('\n');
+            signatureString.Append("(request-target)");
+            signatureString.Append(": ");
+            signatureString.Append(getRequestTarget);
+            signatureString.Append('\n');
+            signatureString.Append("v-c-merchant-id");
+            signatureString.Append(": ");
+            signatureString.Append(merchantId);
+            signatureString.Remove(0, 1);
+
+            byte[] signatureByteString = Encoding.UTF8.GetBytes(signatureString.ToString());
+
+            byte[] decodedKey = Convert.FromBase64String(this.secretKey);
+
+            using (var aKeyId = new HMACSHA256(decodedKey))
+            {
+                byte[] hashmessage = aKeyId.ComputeHash(signatureByteString);
+                string base64EncodedSignature = Convert.ToBase64String(hashmessage);
+
+                signatureHeaderValue.Append("keyid=\"" + this.keyId + "\"");
+                signatureHeaderValue.Append(", algorithm=\"" + algorithm + "\"");
+                signatureHeaderValue.Append(", headers=\"" + getHeaders + "\"");
+                signatureHeaderValue.Append(", signature=\"" + base64EncodedSignature + "\"");
+            }
+
+            return signatureHeaderValue;
+        }
+
+        private static string GenerateDigest(string body)
+        {
+            var digest = "DIGEST_PLACEHOLDER";
+            try
+            {
+                using (var sha256Hash = SHA256.Create())
+                {
+                    byte[] payloadBytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(body));
+                    digest = Convert.ToBase64String(payloadBytes);
+                    digest = "SHA-256=" + digest;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR : " + ex.ToString());
+            }
+
+            return digest;
+        }
+    }
+}
+
---
+++ src/Program.cs
@@ -0,0 +1,149 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+
+namespace FlexApi.Demo
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var cybsHttpClient = new CybsHttpClient();
+
+            var sessionBody = GetSessionBody();
+            var context = cybsHttpClient.Post(sessionBody, "/flex/v2/sessions");
+
+            var handler = new JwtSecurityTokenHandler();
+            context = context.Replace("\"", string.Empty, System.StringComparison.InvariantCultureIgnoreCase);
+            var jsonToken = handler.ReadToken(context);
+            var securityToken = jsonToken as JwtSecurityToken;
+            var flx = securityToken.Claims.First(claim => claim.Type == "flx").Value;
+            var obj = JsonConvert.DeserializeObject<JObject>(flx);
+            var jwk = obj.SelectToken("jwk");
+            var kid = securityToken.Header["kid"];
+
+            var publicKey = cybsHttpClient.Get($"/flex/v2/public-keys/{kid}");
+            var jwk2 = JsonConvert.DeserializeObject<JObject>(publicKey);
+
+            var payload = GetPaymentBody("4111111111111111", context);
+
+            var token = WithJoseJWE(jwk, jwk2, payload, context);
+            var flexToken = cybsHttpClient.Post(token, "/flex/v2/tokens", "application/jwt");
+            Console.WriteLine("WithJoseJWE");
+            Console.WriteLine(flexToken);
+
+            token = WithJoseJWT(jwk, jwk2, payload, context);
+            flexToken = cybsHttpClient.Post(token, "/flex/v2/tokens", "application/jwt");
+            Console.WriteLine("WithJoseJWT");
+            Console.WriteLine(flexToken);
+
+            token = WithChilkat(jwk, jwk2, payload, context);
+            flexToken = cybsHttpClient.Post(token, "/flex/v2/tokens", "application/jwt");
+            Console.WriteLine("WithJoseJWT");
+            Console.WriteLine(flexToken);
+
+            Console.WriteLine("Press any key ...");
+            Console.ReadLine();
+        }
+
+        private static string WithJoseJWE(JToken jwk, JToken jwk2, string payload, string context)
+        {
+            var keys = "{\n" +
+                        "  \"keys\": [\n" +
+                        $"    {jwk.ToString()},\n" +
+                        $"    {jwk2.ToString()}\n" +
+                        "  ]\n" +
+                        "}\n";
+            var keySet = Jose.JwkSet.FromJson(keys, Jose.JWT.DefaultSettings.JsonMapper);
+            var encryptionKey = jwk.SelectToken("kid").ToString();
+
+            var headers = new Dictionary<string, object>();
+            headers.Add("kid", encryptionKey);
+            var recipients = new List<Jose.JweRecipient>();
+            recipients.Add(new Jose.JweRecipient(Jose.JweAlgorithm.RSA_OAEP, keySet.Keys[0], header: headers));
+
+            return Jose.JWE.Encrypt(payload, recipients, Jose.JweEncryption.A256GCM, mode: Jose.SerializationMode.Compact);
+        }
+
+        private static string WithJoseJWT(JToken jwk, JToken jwk2, string payload, string context)
+        {
+            var keys = "{\n" +
+                        "  \"keys\": [\n" +
+                        $"    {jwk.ToString()},\n" +
+                        $"    {jwk2.ToString()}\n" +
+                        "  ]\n" +
+                        "}\n";
+            var keySet = Jose.JwkSet.FromJson(keys, Jose.JWT.DefaultSettings.JsonMapper);
+            var encryptionKey = jwk.SelectToken("kid").ToString();
+
+            var headers = new Dictionary<string, object>();
+            headers.Add("kid", encryptionKey);
+            return Jose.JWT.Encode(payload, keySet.Keys[0], Jose.JweAlgorithm.RSA_OAEP, Jose.JweEncryption.A256GCM, extraHeaders: headers);
+        }
+
+        private static string WithChilkat(JToken jwk, JToken jwk2, string payload, string context)
+        {
+            var jweProtHdr = new Chilkat.JsonObject();
+            jweProtHdr.AppendString("alg", "RSA-OAEP");
+            jweProtHdr.AppendString("enc", "A256GCM");
+            jweProtHdr.AppendString("kid", jwk.SelectToken("kid").ToString());
+
+            var rsaPrivKey = new Chilkat.PrivateKey();
+            var success = rsaPrivKey.LoadJwk(jwk.ToString());
+            Console.WriteLine($"rsaPrivKey.LoadJwk: {success}");
+            var rsaPubKey = rsaPrivKey.GetPublicKey();
+
+            var jwe = new Chilkat.Jwe();
+            jwe.SetProtectedHeader(jweProtHdr);
+            success = jwe.SetPublicKey(0, rsaPubKey);
+
+            return jwe.Encrypt(payload, "utf-8");
+        }
+
+        private static string GetPaymentBody(string number, string context)
+        {
+            return
+                    "{\n" +
+                    "\"data\": {\n" +
+                    "  \"paymentInformation\": {\n" +
+                    "    \"card\": {\n" +
+                    "      \"number\": \"" + number + "\"\n" +
+                    "    }\n" +
+                    "  },\n" +
+                    "},\n" +
+                    "\"context\": \"" + context + "\",\n" +
+                    "\"index\": 0\n" +
+                    "}";
+        }
+
+        private static string GetSessionBody()
+        {
+            var body = "{\n" +
+                        "  \"fields\" : {\n" +
+                        "    \"paymentInformation\" : {\n" +
+                        "      \"card\" : {\n" +
+                        "        \"number\" : { },\n" +
+                        "        \"securityCode\" : {\n" +
+                        "          \"required\" : false\n" +
+                        "        },\n" +
+                        "        \"expirationMonth\" : {\n" +
+                        "          \"required\" : false\n" +
+                        "        },\n" +
+                        "        \"expirationYear\" : {\n" +
+                        "          \"required\" : false\n" +
+                        "        },\n" +
+                        "        \"type\" : {\n" +
+                       "          \"required\" : false\n" +
+                        "        }\n" +
+                        "      }\n" +
+                        "    }\n" +
+                        "  }\n" +
+                        "}";
+            return body;
+        }
+    }
+}
+
